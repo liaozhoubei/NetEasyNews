5489b653a5591d0f7d509e9a9e28fff3ed5f64dc
liaozhoubei <liaozhoubei@outlook.com>
2016-12-24 22:18:40 +0800

    first commit

---
+++ app/src/main/java/cn/bproject/neteasynews/MainActivity.java
@@ -0,0 +1,102 @@
+package cn.bproject.neteasynews;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.bproject.neteasynews.bean.Tab;
+import cn.bproject.neteasynews.fragment.AboutFragment;
+import cn.bproject.neteasynews.fragment.NewsFragment;
+import cn.bproject.neteasynews.fragment.PhotoFragment;
+import cn.bproject.neteasynews.fragment.VideoFragment;
+import cn.bproject.neteasynews.widget.FragmentTabHost;
+
+public class MainActivity extends AppCompatActivity {
+    private FragmentTabHost mTabHost;
+    private LayoutInflater mInflater;
+    private List<Tab> mTabs = new ArrayList<>(5);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolbar();
+        initTab();
+
+    }
+
+    // 初始化Toolbar
+    private void initToolbar(){
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    // 初始化底部标签栏
+    private void initTab() {
+        // 新闻标签
+        Tab tab_news = new Tab(NewsFragment.class,R.string.news_fragment,R.drawable.select_icon_news);
+        // 图片标签
+        Tab tab_photo = new Tab(PhotoFragment.class,R.string.photo_fragment,R.drawable.select_icon_photo);
+        // 视频标签
+        Tab tab_video = new Tab(VideoFragment.class,R.string.video_fragment,R.drawable.select_icon_video);
+        // 我 标签
+        Tab tab_about = new Tab(AboutFragment.class,R.string.about_fragment,R.drawable.select_icon_about);
+
+
+        mTabs.add(tab_news);
+        mTabs.add(tab_photo);
+        mTabs.add(tab_video);
+        mTabs.add(tab_about);
+
+
+        // 设置FragmentTab
+        mInflater = LayoutInflater.from(this);
+        mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+
+        for (Tab tab : mTabs){
+
+            TabHost.TabSpec tabSpec = mTabHost.newTabSpec(getString(tab.getTitle()));
+
+            tabSpec.setIndicator(buildIndicator(tab));
+
+            mTabHost.addTab(tabSpec,tab.getFragment(),null);
+
+        }
+
+        mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+
+
+            }
+        });
+
+        mTabHost.getTabWidget().setShowDividers(LinearLayout.SHOW_DIVIDER_NONE);
+        mTabHost.setCurrentTab(0);
+    }
+
+    // 设置底部tab的图片和文字
+    private View buildIndicator(Tab tab){
+
+        View view = mInflater.inflate(R.layout.tab_indicator, null);
+        ImageView img = (ImageView) view.findViewById(R.id.icon_tab);
+        TextView text = (TextView) view.findViewById(R.id.txt_indicator);
+
+        img.setBackgroundResource(tab.getIcon());
+        text.setText(tab.getTitle());
+
+        return  view;
+    }
+}
---
+++ app/src/main/java/cn/bproject/neteasynews/fragment/VideoFragment.java
@@ -0,0 +1,23 @@
+package cn.bproject.neteasynews.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2016/12/24.
+ */
+
+public class VideoFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText("VideoFragment");
+        return textView;
+    }
+}
