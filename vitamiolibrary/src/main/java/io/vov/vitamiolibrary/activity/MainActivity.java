0dd9c19878cca0ccf34b8149bff51d48df34a986
liaozhoubei <lzb19890920@sina.com>
2017-01-03 21:57:40 +0800

    Add Vitamio

---
+++ vitamiolibrary/src/main/java/io/vov/vitamiolibrary/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package io.vov.vitamiolibrary.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.vov.vitamiolibrary.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
