5e41612aeaabc2c0a047218c06c0da2df0fb1c96
liaozhoubei <lzb19890920@sina.com>
2017-02-16 16:53:08 +0800

    修改bug

--- app/src/main/java/cn/bproject/neteasynews/activity/SplashActivity.java
+++ app/src/main/java/cn/bproject/neteasynews/activity/SplashActivity.java
@@ -18,6 +18,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(intent);
+                finish();
             }
         }, 3000);
     }
