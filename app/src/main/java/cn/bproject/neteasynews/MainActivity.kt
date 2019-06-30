6da939ae131db1622b6fd4a217dd753a29277a64
bei <liaozhoubei@gmail.com>
2019-06-30 18:32:41 +0800

    add versions.gradle

--- app/src/main/java/cn/bproject/neteasynews/MainActivity.kt
+++ app/src/main/java/cn/bproject/neteasynews/MainActivity.kt
@@ -2,11 +2,13 @@ package cn.bproject.neteasynews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var gson: Gson = Gson()
     }
 }
